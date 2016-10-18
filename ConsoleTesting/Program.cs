2b416930a1443bec0ca47c5c879dce91a752f149
stg <stg@iastate.edu>
2016-10-18 15:42:09 -0500

    Fixing mistakes

---
+++ ConsoleTesting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing;
+
+namespace ConsoleTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var consoleGame = new ConsoleGame();
+            consoleGame.Play();
+        }
+    }
+}
+
+
