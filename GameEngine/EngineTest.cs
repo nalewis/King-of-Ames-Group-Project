8f97291d45cb820d16689487a550c06f25f73fe7
rapalmer <rapalmer@iastate.edu>
2016-10-19 22:33:09 -0500

    Finished organizing new develop branch

---
+++ GameEngine/EngineTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GameEngine
+{
+#if WINDOWS || LINUX
+    public static class EngineTest
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Engine())
+                game.Run();
+        }
+    }
+#endif
+}
