e1fa1cf2fca661f93760cf28e94e8bcc54053d06
rapalmer <rapalmer@iastate.edu>
2016-11-01 21:49:45 -0500

    added back needed files after last change

---
+++ GameEngine/DiceGraphics/DiceImage.cs
@@ -0,0 +1,60 @@
+﻿using GamePieces.Dice;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameEngine.DiceGraphics
+{
+    class DiceImage
+    {
+        protected Texture2D currentFace;
+        protected Die die;
+
+        public DiceImage(Die die)
+        {
+            this.die = die;
+            updateFromDie();
+            
+        }
+
+        public void updateFromDie()
+        {
+            switch (die.Symbol)
+            {
+                case Symbol.One:
+                    changeFace("dice1");
+                    break;
+
+                case Symbol.Two:
+                    changeFace("dice2");
+                    break;
+
+                case Symbol.Three:
+                    changeFace("dice3");
+                    break;
+
+                case Symbol.Heal:
+                    changeFace("diceHealth");
+                    break;
+
+                case Symbol.Attack:
+                    changeFace("diceAttack");
+                    break;
+
+                case Symbol.Energy:
+                    changeFace("diceEnergy");
+                    break;
+            }
+        }
+
+        protected void changeFace(string newFace)
+        {
+            Texture2D getTexture;
+            Engine.textureList.TryGetValue(newFace, out getTexture);
+            currentFace = getTexture;
+        }
+
+        public Texture2D getCurrentFace()
+        {
+            return currentFace;
+        }
+    }
+}
