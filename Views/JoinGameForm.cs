0dd0fd73acc9b493f332d82779f0c6c1f9f7bb1c
Nick Lewis <nalewis@iastate.edu>
2016-11-02 00:57:43 -0600

    Migrated forms to views project

--- Views/JoinGameForm.cs
+++ Views/JoinGameForm.cs
@@ -1,5 +1,4 @@
-﻿using Controllers.test;
-using System;
+﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
 
---
+++ Views/SignupForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Networking;
+using Controllers.Helpers;
+
+namespace LoginScreenWinForm
+{
+    //TODO add error message checking
+    public partial class NewUserForm : Form
+    {
+        public NewUserForm()
+        {
+            InitializeComponent();
+        }
+
+        private void newUserButton_Click(object sender, EventArgs e)
+        {
+            if (newUsername.TextLength > 0 && newPassword.TextLength > 0)
+            {
+                //hide label from previous failures
+                errorLabel.Hide();
+                //NewUser.handleUserInput(newUsername.Lines[0], newPassword.Lines[0], errorLabel);
+                var result = NetworkClasses.createUser(newUsername.Lines[0], newPassword.Lines[0], Helpers.GetLocalIPAddress());
+                Console.WriteLine(result);
+            }
+        }
+
+        private void loginLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form form = new LoginForm();
+            form.Show();
+            this.Hide();
+        }
+    }
+}
