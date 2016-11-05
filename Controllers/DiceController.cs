9fa007882d7770cba423b03da0c1202246890863
rapalmer <rapalmer@iastate.edu>
2016-11-04 21:28:21 -0500

    Brought controllers up to date with develop

--- Controllers/DiceController.cs
+++ Controllers/DiceController.cs
@@ -1,23 +1,54 @@
-﻿using GamePieces.Dice;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using GamePieces.Dice;
+using GamePieces.Session;
 
 namespace Controllers
 {
     public static class DiceController
     {
-        public static List<Die> getDice()
+        /// <summary>
+        /// Gets all of the dice being rolled
+        /// </summary>
+        /// <returns>Dice</returns>
+        public static List<Die> GetDice()
         {
-            return GamePieces.Session.DiceRoller.Rolling;
+            return DiceRoller.Rolling;
         }
 
-        public static void setup(int numDice)
+        /// <summary>
+        /// Save the die at the given index
+        /// </summary>
+        /// <param name="index">Index</param>
+        public static void SaveDie(int index)
         {
-            GamePieces.Session.DiceRoller.Setup(numDice);
+            if (index < 0 || index > DiceRoller.Rolling.Count) return;
+            DiceRoller.Rolling[index].Save = true;
         }
 
-        public static void roll()
+        /// <summary>
+        /// Un-save the die at the given index
+        /// </summary>
+        /// <param name="index">Index</param>
+        public static void UnSaveDie(int index)
         {
-            GamePieces.Session.DiceRoller.Roll();
+            if (index < 0 || index > DiceRoller.Rolling.Count) return;
+            DiceRoller.Rolling[index].Save = false;
+        }
+
+        /// <summary>
+        /// Roll the dice
+        /// </summary>
+        public static void Roll()
+        {
+            Game.Roll();
+        }
+
+        /// <summary>
+        /// End rolling dice
+        /// </summary>
+        public static void EndRolling()
+        {
+            Game.EndRolling();
         }
     }
 }
