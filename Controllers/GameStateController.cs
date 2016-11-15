fc855a38e9cbc086414b88cd994847383c5009d4
Sam Gutermuth <stg@iastate.edu>
2016-11-15 14:01:56 -0600

    Added action data packets

--- Controllers/GameStateController.cs
+++ Controllers/GameStateController.cs
@@ -1,17 +1,103 @@
-﻿using GamePieces.Session;
+﻿using GamePieces.Cards;
+using GamePieces.Session;
+using Networking.Actions;
 
 namespace Controllers
 {
     public static class GameStateController
     {
-        public static void StartTurn()
+        public static void AcceptAction(ActionPacket actionPacket)
         {
-            Game.StartTurn();
+            switch (actionPacket.Action)
+            {
+                case Action.StartTurn:
+                    Game.StartTurn();
+                    break;
+                case Action.Roll:
+                    DiceController.Roll();
+                    break;
+                case Action.EndRolling:
+                    DiceController.EndRolling();
+                    break;
+                case Action.BuyCard:
+                    switch ((CardsForSale) actionPacket.Value)
+                    {
+                        case CardsForSale.One:
+                            CardController.BuyCardOne();
+                            break;
+                        case CardsForSale.Two:
+                            CardController.BuyCardTwo();
+                            break;
+                        case CardsForSale.Three:
+                            CardController.BuyCardThree();
+                            break;
+                        default:
+                            return;
+                    }
+                    break;
+                case Action.RemoveCard:
+                    Game.RemoveCard((Card) actionPacket.Value);
+                    break;
+                case Action.EndTurn:
+                    Game.EndTurn();
+                    break;
+                case Action.Yield:
+                    MonsterController.GetById(actionPacket.PlayerId).Yield();
+                    break;
+                case Action.SaveDie:
+                    DiceController.SaveDie((int) actionPacket.Value);
+                    break;
+                case Action.UnSaveDie:
+                    DiceController.UnSaveDie((int) actionPacket.Value);
+                    break;
+                default:
+                    return;
+            }
         }
 
-        public static void EndTurn()
+        public static ActionPacket StartTurn()
         {
-            Game.EndTurn();
+            return new ActionPacket(Action.StartTurn);
+        }
+
+        public static ActionPacket Roll()
+        {
+            return new ActionPacket(Action.Roll);
+        }
+
+        public static ActionPacket EndRolling()
+        {
+            return new ActionPacket(Action.EndRolling);
+        }
+
+        public static ActionPacket BuyCard(CardsForSale cardsForSale)
+        {
+            return new ActionPacket(Action.BuyCard, value: cardsForSale);
+        }
+
+        public static ActionPacket RemoveCard()
+        {
+            return new ActionPacket(Action.RemoveCard);
+        }
+
+        public static ActionPacket EndTurn()
+        {
+            return new ActionPacket(Action.EndTurn);
+        }
+
+        public static ActionPacket Yield(int playerId)
+        {
+            return new ActionPacket(Action.Yield, playerId);
+        }
+
+        public static ActionPacket SaveDie(int index)
+        {
+            return new ActionPacket(Action.SaveDie, value: index);
+        }
+
+        public static ActionPacket UnSaveDie(int index)
+        {
+            return new ActionPacket(Action.UnSaveDie, value: index);
         }
     }
 }
