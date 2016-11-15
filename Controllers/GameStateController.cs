94d1a53c5ad3021afd6414854ba2ee1140ea6983
Sam Gutermuth <stg@iastate.edu>
2016-11-15 14:15:50 -0600

    Documentation

--- Controllers/GameStateController.cs
+++ Controllers/GameStateController.cs
@@ -6,8 +6,13 @@ namespace Controllers
 {
     public static class GameStateController
     {
+        /// <summary>
+        /// Changes the game state with the given action.
+        /// </summary>
+        /// <param name="actionPacket">Action packet</param>
         public static void AcceptAction(ActionPacket actionPacket)
         {
+            if(!Game.Host) return;
             switch (actionPacket.Action)
             {
                 case Action.StartTurn:
@@ -55,46 +60,86 @@ namespace Controllers
             }
         }
 
+        /// <summary>
+        /// Start turn.
+        /// </summary>
+        /// <returns>Start turn action packet</returns>
         public static ActionPacket StartTurn()
         {
             return new ActionPacket(Action.StartTurn);
         }
 
+        /// <summary>
+        /// Roll.
+        /// </summary>
+        /// <returns>Roll action packet</returns>
         public static ActionPacket Roll()
         {
             return new ActionPacket(Action.Roll);
         }
 
+        /// <summary>
+        /// End Rolling.
+        /// </summary>
+        /// <returns>End rolling action packet</returns>
         public static ActionPacket EndRolling()
         {
             return new ActionPacket(Action.EndRolling);
         }
 
+        /// <summary>
+        /// Buy card.
+        /// </summary>
+        /// <param name="cardsForSale">Card for sale.</param>
+        /// <returns>Buy card action packet</returns>
         public static ActionPacket BuyCard(CardsForSale cardsForSale)
         {
             return new ActionPacket(Action.BuyCard, value: cardsForSale);
         }
 
+        /// <summary>
+        /// Remove card.
+        /// </summary>
+        /// <returns>Remove card action packet</returns>
         public static ActionPacket RemoveCard()
         {
             return new ActionPacket(Action.RemoveCard);
         }
 
+        /// <summary>
+        /// End turn.
+        /// </summary>
+        /// <returns>End turn action packet</returns>
         public static ActionPacket EndTurn()
         {
             return new ActionPacket(Action.EndTurn);
         }
 
+        /// <summary>
+        /// Yield.
+        /// </summary>
+        /// <param name="playerId">Player Id</param>
+        /// <returns>Yield action packet</returns>
         public static ActionPacket Yield(int playerId)
         {
             return new ActionPacket(Action.Yield, playerId);
         }
 
+        /// <summary>
+        /// Save die.
+        /// </summary>
+        /// <param name="index">Index</param>
+        /// <returns>Save die action packet</returns>
         public static ActionPacket SaveDie(int index)
         {
             return new ActionPacket(Action.SaveDie, value: index);
         }
 
+        /// <summary>
+        /// Un-save die.
+        /// </summary>
+        /// <param name="index">Index</param>
+        /// <returns>Un-save action packet</returns>
         public static ActionPacket UnSaveDie(int index)
         {
             return new ActionPacket(Action.UnSaveDie, value: index);
