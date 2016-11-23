c93c26c804d371fb7fcc3d2ce372d8ab8fe186db
Nick Lewis <nalewis@iastate.edu>
2016-11-23 11:50:17 -0600

    updating richard's code for cards

--- GameEngine/GameScreens/MainGameScreen.cs
+++ GameEngine/GameScreens/MainGameScreen.cs
@@ -284,7 +284,7 @@ namespace GameEngine.GameScreens
 
             if (Engine.InputManager.KeyPressed(Keys.Y))
             {
-                ScreenManager.AddScreen(new BuyCards(Client.cardNames, MonsterController.GetById(_localPlayer).Energy));
+                ScreenManager.AddScreen(new BuyCards(MonsterController.GetById(_localPlayer).Energy));
                 if (cardScreenChoice == -1) return;
                 var cfs = CardsForSale.One;
                 switch (cardScreenChoice)
