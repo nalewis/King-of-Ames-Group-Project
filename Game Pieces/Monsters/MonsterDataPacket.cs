f58f33a5e2c305b5b5ee61f33913b991e0df735e
Sam Gutermuth <stg@iastate.edu>
2016-12-06 00:39:00 -0600

    fixed monster card sending

--- Game Pieces/Monsters/MonsterDataPacket.cs
+++ Game Pieces/Monsters/MonsterDataPacket.cs
@@ -9,7 +9,7 @@ namespace GamePieces.Monsters
         public int PlayerId { get; set; }
         public string Name { get; set; }
         public Location Location { get; set; }
-        public Card[] Cards { get; set; }
+        public CardDataPacket[] Cards { get; set; }
         public int NumberOfCards { get; set; }
         public int PreviousNumberOfCards { get; set; }
         public int Energy { get; set; }
@@ -26,7 +26,7 @@ namespace GamePieces.Monsters
         public bool CanYield { get; set; }
         public State State { get; set; }
 
-        public MonsterDataPacket(int playerId, string name, Location location, Card[] cards,
+        public MonsterDataPacket(int playerId, string name, Location location, CardDataPacket[] cards,
             int numberOfCards, int previousNumberOfCards, int energy, int previousEnergy, int victoryPoints,
             int previousVictoryPoints, int health, int previousHealth, int maximumHealth, int attackPoints, int dice,
             int maximumRolls, int remainingRolls, bool canyield, State state)
