4379091cf812dc83eeb534e4f926b0bddc39aba3
Nick Lewis <nalewis@iastate.edu>
2016-12-05 20:52:17 -0600

    attempting to fix start turn

--- GameEngine/GameScreens/MainGameScreen.cs
+++ GameEngine/GameScreens/MainGameScreen.cs
@@ -90,7 +90,7 @@ namespace GameEngine.GameScreens
 
             if (!MonsterController.IsDead(_localPlayer))    //If a player isn't dead check for their startOfTurn
             {
-                if (_gameState != GameState.Spectating && MonsterController.State(_localPlayer) == State.StartOfTurn)
+                if (_gameState != GameState.Spectating && Client.IsStart)
                     if(MonsterController.CanYield(_localPlayer) == false)
                         _gameState = GameState.StartTurn;
             }
@@ -211,12 +211,14 @@ namespace GameEngine.GameScreens
             {   
                 _gameState = GameState.Rolling;
                 Client.SendActionPacket(GameStateController.Roll());
-                System.Threading.Thread.Sleep(500);
+                System.Threading.Thread.Sleep(50);
                 _diceRow.AddDice(DiceController.GetDice());
                 _diceRow.Hidden = false;
                 RollingDice.AddDice(DiceController.GetDice());
                 RollingDice.Hidden = false;
             }
+
+            Client.IsStart = false;
         }
 
         /// <summary>
